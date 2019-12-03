4a5b6d256ffd1a113536ecb348f46127640bc496
Macc Liu <maccliu@qq.com>
2019-12-03 22:27:34 +0800

    init

---
+++ _bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Dida Framework  -- PHP轻量级快速开发框架
+ * 版权所有 (c) 上海宙品信息科技有限公司
+ *
+ * 官网: <https://github.com/zeupin/dida>
+ * Gitee: <https://gitee.com/zeupin/dida>
+ */
+require "D:/Projects/github/dida/src/Dida/Autoloader.php";
+
+Dida\Autoloader::addPsr4("Dida", "D:/Projects/github/dida/src/Dida");
+
+
+function test_section($title)
+{
+    echo "\n/**************************  $title  **************************/\n";
+}
